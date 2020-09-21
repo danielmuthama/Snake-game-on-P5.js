e53033eee5f335e065a5530e4224eb9117e602b9
Daniel Muthama <40577986+danielmuthama@users.noreply.github.com>
2020-09-21 16:08:59 -0700

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,9 +1,3 @@
-// Daniel Shiffman
-// http://youtube.com/thecodingtrain
-// http://codingtra.in
-
-// Coding Challenge #115: Snake Game Redux
-// https://youtu.be/OMoVcohRgZA
 
 class Snake {
   constructor() {
